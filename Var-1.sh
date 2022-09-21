334c75a95178b7a5edd68c766d9ab23ae1b23ff5
Mahesh <mahimathwale@gmail.com>
2022-09-21 20:29:12 +0530

    Variable added

---
+++ Var-1.sh
@@ -0,0 +1,15 @@
+a=10
+b=Santa
+c=true
+
+echo Value of a = $a
+
+#command Substitute
+
+DATE=$(date)
+echo Today Date is $DATE
+
+#Arithmatic Substitute
+
+ADD=$((2+3+4))
+echo addition of 2+3+4 is $ADD
