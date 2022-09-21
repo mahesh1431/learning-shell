dc3396a0a5b6c40afa3e71a006edb1ab1d60fe07
Mahesh <mahimathwale@gmail.com>
2022-09-21 20:00:35 +0530

    Adding front end shell script Chages made on lines Exit status code added

--- Frontend.sh
+++ Frontend.sh
@@ -1,12 +1,19 @@
 amazon-linux-extras list | grep nginx
+echo status = $?
 amazon-linux-extras enable nginx1
+echo status = $?
 sudo yum clean metadata
+echo status = $?
 yum -y install nginx
+echo status = $?
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+ echo status = $?
  cd /usr/share/nginx/html
  rm -rf *
  unzip /tmp/frontend.zip
  mv frontend-main/static/* .
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
  systemctl enable nginx
+ echo status = $?
  systemctl restart nginx
+ echo status = $?
