660b0a4ddc6e25d76246627de5916ba1f8a1e71d
Mahesh <mahimathwale@gmail.com>
2022-09-28 23:41:24 +0530

    Variable Started

--- Mongodb.sh
+++ Mongodb.sh
@@ -8,7 +8,7 @@ yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
 echo "MongoDB Listen Addres"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 echo status = $?
 
 echo "MongoDB service start"
---
+++ Variable/01-var.sh
@@ -0,0 +1,5 @@
+a=10
+b=20
+c=true
+
+echo value of a is = $a
